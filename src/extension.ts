1bc842e4fdee50e57340a37ecfc25891831759ed
khmoryz <kokkenro0520@gmail.com>
2025-01-04 17:27:49 +0900

    feat: Implement rain deploy with active file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,15 +9,32 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-hello-world" is now active!');
+	console.log('Congratulations, your extension "vscode-rain" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vs-hello-world.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vs-hello-world!');
+	const disposable = vscode.commands.registerCommand('vscode-rain.deploy', () => {
+		// Execute `rain deploy` with active file path after inputing the stack name
+        vscode.window
+          .showInputBox({
+            prompt: "Enter the stack name",
+            placeHolder: "Stack name",
+          })
+          .then((stackName) => {
+            if (stackName) {
+              const activeTextEditor = vscode.window.activeTextEditor;
+              if (activeTextEditor) {
+                const filePath = activeTextEditor.document.fileName;
+                vscode.window.showInformationMessage(`Do you want to deploy ${filePath} to stack ${stackName}?`, "Yes", "No").then((selection) => {
+                  if (selection === "Yes") {
+                    const terminal = vscode.window.createTerminal(`Rain Deploy: ${stackName}`);
+                    terminal.sendText(`rain deploy ${filePath} ${stackName}`);
+                    terminal.show();
+                  }
+                });
+              } else {
+                vscode.window.showErrorMessage("No active file found");
+              }
+            }
+          });
 	});
 
 	context.subscriptions.push(disposable);
